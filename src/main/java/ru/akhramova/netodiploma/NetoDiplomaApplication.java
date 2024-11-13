711c5d8c9cf96aeddf675cf3d5b4d03aeed40c0a
Anna Khramova <xaa2123@mail.ru>
2024-11-13 15:39:49 +0300

    Initial commit

---
+++ src/main/java/ru/akhramova/netodiploma/NetoDiplomaApplication.java
@@ -0,0 +1,13 @@
+package ru.akhramova.netodiploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetoDiplomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetoDiplomaApplication.class, args);
+    }
+
+}
